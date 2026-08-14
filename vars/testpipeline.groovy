a76cdf8c34b4e8642ad9c406beb6e36d409bc6ba
dharma0907 <dharma.akdm@gmail.com>
2026-08-14 15:26:42 +0530

    adding Jenkins files

--- vars/testpipeline.groovy
+++ vars/testpipeline.groovy
@@ -2,16 +2,17 @@
 def call (Map configMap){
     pipeline {
         agent any
-        // environment {
-        //     project = configMap.get("project")
-        //     component = configMap.get("component")
-        // }
+        environment {
+            project = configMap.get("project")
+            component = configMap.get("component")
+        }
         stages {
             stage('Build') {
                 steps {
                     script {
                         sh """
                             echo 'Building..'
+                            echo "Project: ${project}, component: ${component}"
                            
                         """
                     }
