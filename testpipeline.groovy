764aa7ba09760c8b977a760b5f09ba425a34e88a
dharma0907 <dharma.akdm@gmail.com>
2026-08-14 14:55:03 +0530

    adding Jenkins files

---
+++ testpipeline.groovy
@@ -0,0 +1,33 @@
+// This is function, by default if some one calls this pipeline like testPipeline call function will be executed
+def call (Map configMap){
+    pipeline {
+        agent any
+        // environment {
+        //     project = configMap.get("project")
+        //     component = configMap.get("component")
+        // }
+        stages {
+            stage('Build') {
+                steps {
+                    script {
+                        sh """
+                            echo 'Building..'
+                           
+                        """
+                    }
+                    
+                }
+            }
+            stage('Test') {
+                steps {
+                    echo 'Testing..'
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    echo 'Deploying....'
+                }
+            }
+        }
+    }
+}
