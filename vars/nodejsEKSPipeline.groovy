c3b18f99a473e6f466dcca92f647e60e8e5bfd3a
dharma0907 <dharma.akdm@gmail.com>
2026-08-18 23:05:24 +0530

    adding Jenkins files

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -141,24 +141,23 @@ def call(configMap) {
             steps {
                 
                 script {
-                    try{
+                        try{
 
-                       // 'aws-global-creds' is the ID of your Jenkins credential entry
-                        withAWS(credentials: 'aws-creds', region: 'us-east-1') {
-                            sh """
-                            aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                            docker build -t ${project}/${component}:${appVersion} .
-                            docker tag ${project}/${component}:${appVersion} ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion}
-                          
-                            """
-                            utils.updateCommitStatus("success", "Build docker success", "build-image")
+                        // 'aws-global-creds' is the ID of your Jenkins credential entry
+                            withAWS(credentials: 'aws-creds', region: 'us-east-1') {
+                                sh """
+                                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                                docker build -t ${project}/${component}:${appVersion} .
+                                docker tag ${project}/${component}:${appVersion} ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion}
+                            
+                                """
+                                utils.updateCommitStatus("success", "Build docker success", "build-image")
+                            }
                         }
                         catch(Exception e){
-                            utils.updateCommitStatus("failure", "image build failed", "build-image")
-                            throw e
+                                utils.updateCommitStatus("failure", "image build failed", "build-image")
+                                throw e
                         } 
-
-
                     } 
                        
                 }        
