69baf8bf055538eb6d3ee4b1cf96b9a2b16cb0d7
dharma0907 <dharma.akdm@gmail.com>
2026-08-19 13:01:40 +0530

    adding Jenkins files

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -146,9 +146,9 @@ def call(configMap) {
                         // 'aws-global-creds' is the ID of your Jenkins credential entry
                             withAWS(credentials: 'aws-creds', region: 'us-east-1') {
                                 sh """
-                                aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
-                                docker build -t ${project}/${component}:${appVersion} .
-                                docker tag ${project}/${component}:${appVersion} ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion}
+                                    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                                    docker build -t ${project}/${component}:${appVersion} .
+                                    docker tag ${project}/${component}:${appVersion} ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${project}/${component}:${appVersion}
                             
                                 """
                                 
